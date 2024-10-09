29bd5a54712c5e022ccad1e43bb466f57c66fa35
Walker Araújo Ataide <walkerataide@gmail.com>
2024-10-09 14:37:19 -0300

    add input.py

---
+++ input.py
@@ -0,0 +1,10 @@
+# Solicita os dados do ususário
+nome = input("Digite seu nome:")
+escola = input("Digite a sua escola:")
+# Texto Formatado
+print(f"Olá, {nome}, do(a) {escola}. Bem-vindo ao nosso sistema!")
+
+# print("Olá, ")
+# print(resposta)
+
+# print("Olá, ", resposta)
